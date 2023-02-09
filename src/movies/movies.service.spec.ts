074eb140b53af3167bd0820afbb8b79190c0ad65
unknown <kid07118@naver.com>
2023-02-09 13:43:59 +0900

    feat: service unit test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import exp from 'constants';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -15,4 +17,34 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('getAll', () => {
+    it("should return an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    })
+  })
+
+  describe('getOne', () => {
+    it("should return a movie", () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000
+      });
+
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      // expect(movie.id).toEqual(1);
+    })
+
+    it("should throw 404 error", () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        // expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    })
+  })
 });
